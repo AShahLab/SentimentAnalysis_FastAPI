47bc104d432a42df145817d89f7c8a9a6f874ff6
angelikawagner <angelika.wagner@hotmail.com>
2022-05-07 15:47:44 -0500

    sentiment endpoint

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from fastapi import FastAPI
+from transformers import pipeline
+
+from pydantic import BaseModel
+
+class PredictionRequest(BaseModel):
+  query_string: str
+
 app = FastAPI()
+
+sentiment_model = pipeline("sentiment-analysis")
+
 @app.get("/health")
 def health():
     return "Service is online."
+
+
+@app.post("/my-endpoint")
+def my_endpoint(request: PredictionRequest):
+    sentiment_query_sentence = request.query_string
+    sentiment = sentiment_model(sentiment_query_sentence)
+    return sentiment
